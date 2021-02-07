41fa2bbb731c41e59775433c5e8788fc994e99f4
Gabriel Pettier <gabriel.pettier@gmail.com>
2021-02-07 23:59:55 +0100

    Add tests for f-string support in kvlang

--- kivy/tests/test_lang.py
+++ kivy/tests/test_lang.py
@@ -308,6 +308,67 @@ class LangTestCase(unittest.TestCase):
         assert root.text == 'é 😊'
         os.close(fd)
 
+    def test_bind_fstring(self):
+        from kivy.lang import Builder
+        label = Builder.load_string(dedent('''
+        <TestLabel@Label>:
+            text: f'{self.pos}|{self.size}'
+        TestLabel:
+        '''))
+        assert label.text == '[0, 0]|[100, 100]'
+        label.pos = 150, 200
+        assert label.text == '[150, 200]|[100, 100]'
+
+    def test_bind_fstring_reference(self):
+        from kivy.lang import Builder
+        root = Builder.load_string(dedent('''
+        FloatLayout:
+            Label:
+                id: original
+                text: 'perfect'
+            Label:
+                id: duplicate
+                text: f'{original.text}'
+        '''))
+        assert root.ids.duplicate.text == 'perfect'
+        root.ids.original.text = 'new text'
+        assert root.ids.duplicate.text == 'new text'
+
+    def test_bind_fstring_expressions(self):
+        from kivy.lang import Builder
+        root = Builder.load_string(dedent('''
+        FloatLayout:
+            Label:
+                id: original
+                text: 'perfect'
+            Label:
+                id: target1
+                text: f"{' '.join(p.upper() for p in original.text)}"
+            Label:
+                id: target2
+                text: f"{''.join(sorted({p.upper() for p in original.text}))}"
+            Label:
+                id: target3
+                text: f"{'odd' if len(original.text) % 2 else 'even'}"
+            Label:
+                id: target4
+                text: f"{original.text[len(original.text) // 2:]}"
+            Label:
+                id: target5
+                text: f"{not len(original.text) % 2}"
+        '''))
+        assert root.ids.target1.text == 'P E R F E C T'
+        assert root.ids.target2.text == 'CEFPRT'
+        assert root.ids.target3.text == 'odd'
+        assert root.ids.target4.text == 'fect'
+        assert root.ids.target5.text == 'False'
+        root.ids.original.text = 'new text'
+        assert root.ids.target1.text == 'N E W   T E X T'
+        assert root.ids.target2.text == ' ENTWX'
+        assert root.ids.target3.text == 'even'
+        assert root.ids.target4.text == 'text'
+        assert root.ids.target5.text == 'True'
+
 
 if __name__ == '__main__':
     unittest.main()
